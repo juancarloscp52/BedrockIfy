22395f0b94b6de9d539e086f60a31e37526a6829
juancarloscp52 <juancarloscp52@gmail.com>
2020-09-11 17:00:28 +0200

    Added Idle Hand Animations.

---
+++ src/main/java/me/juancarloscp52/bedrockify/client/mixin/HeldItemRendererMixin.java
@@ -0,0 +1,31 @@
+package me.juancarloscp52.bedrockify.client.mixin;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(HeldItemRenderer.class)
+public class HeldItemRendererMixin {
+
+    int timer = 0;
+
+    /**
+     * Adds "breathing" idle animation to items in hand.
+     */
+    @Inject(method = "applyEquipOffset", at=@At("HEAD"),cancellable = true)
+    public void applyEquipOffset (MatrixStack matrices, Arm arm, float equipProgress, CallbackInfo info){
+        int i = arm == Arm.RIGHT ? 1 : -1;
+        double breath = (i==1 ? MathHelper.sin(timer/200f) : MathHelper.cos(timer/200f))*0.01D;
+        matrices.translate(((float)i * 0.56F), (-0.52F + equipProgress * -0.6F) + breath, -0.7200000286102295D);
+        timer ++;
+        info.cancel();
+    }
+
+}
