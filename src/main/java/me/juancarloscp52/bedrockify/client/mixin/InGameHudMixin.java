29fdd853a6fcc27b5e5bc674779897f23f933d81
juancarloscp52 <juancarloscp52@gmail.com>
2020-10-10 18:18:16 +0200

    Fixed Graphic bug with hotbar when Main Hand is set to left.

--- src/main/java/me/juancarloscp52/bedrockify/client/mixin/InGameHudMixin.java
+++ src/main/java/me/juancarloscp52/bedrockify/client/mixin/InGameHudMixin.java
@@ -61,12 +61,12 @@ public abstract class InGameHudMixin extends DrawableHelper {
      */
     @Redirect(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
     private void drawTextureHotbar(InGameHud inGameHud, MatrixStack matrices, int x, int y, int u, int v, int width, int height) {
-        if (width == 182 || x == (scaledWidth / 2) - 91 - 29) {
+        if((width ==29 && height == 24) || width == 182){
             RenderSystem.color4f(1.0F, 1.0F, 1.0F, 0.6F);
             inGameHud.drawTexture(matrices, x, y - screenBorder, u, v, width, height);
             RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1);
-        } else {
-            inGameHud.drawTexture(matrices, x, y - screenBorder, u, v, width, height + 2);
+        }else{
+            inGameHud.drawTexture(matrices, x, y - screenBorder, u, v, width, width == 24 ? height+2 : height);
         }
     }
 
