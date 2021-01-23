9df4dd08bf30af943e570d7ed6bcdea9cea8a3c9
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-01-23 20:36:14 +0200

    Properly fix items being unusable

--- src/main/java/me/juancarloscp52/bedrockify/client/features/quickArmorSwap/ArmorReplacer.java
+++ src/main/java/me/juancarloscp52/bedrockify/client/features/quickArmorSwap/ArmorReplacer.java
@@ -17,22 +17,20 @@ public class ArmorReplacer {
 
     public static TypedActionResult<ItemStack> tryChangeArmor(PlayerEntity playerEntity, Hand hand){
         ItemStack newArmor = playerEntity.getStackInHand(hand);
+        if (newArmor.getItem() == Items.ELYTRA || newArmor.getItem() instanceof ArmorItem){
             ClientPlayerInteractionManager interactionManager = MinecraftClient.getInstance().interactionManager;
             if(interactionManager != null && MinecraftClient.getInstance().mouse.wasRightButtonClicked()){
-                if (!(newArmor.getItem() == Items.ELYTRA || newArmor.getItem() instanceof ArmorItem))
-                    return TypedActionResult.pass(newArmor);
-
                 EquipmentSlot equipmentSlot = MobEntity.getPreferredEquipmentSlot(newArmor);
 
-                if(playerEntity.getEquippedStack(equipmentSlot).getItem().equals(Items.AIR))
-                    return TypedActionResult.pass(newArmor);
-
-                int slotIndex = 8-equipmentSlot.getEntitySlotId();
-                interactionManager.clickSlot(playerEntity.playerScreenHandler.syncId,slotIndex,playerEntity.inventory.main.indexOf(newArmor), SlotActionType.SWAP,playerEntity);
-                playerEntity.playSound(newArmor.getItem() == Items.ELYTRA ? SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA : ((ArmorItem)newArmor.getItem()).getMaterial().getEquipSound(), 1.0F,1.0F);
-                return TypedActionResult.success(playerEntity.getStackInHand(hand));
+                if(!playerEntity.getEquippedStack(equipmentSlot).getItem().equals(Items.AIR)){
+                    int slotIndex = 8-equipmentSlot.getEntitySlotId();
+                    interactionManager.clickSlot(playerEntity.playerScreenHandler.syncId,slotIndex,playerEntity.inventory.main.indexOf(newArmor), SlotActionType.SWAP,playerEntity);
+                    playerEntity.playSound(newArmor.getItem() == Items.ELYTRA ? SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA : ((ArmorItem)newArmor.getItem()).getMaterial().getEquipSound(), 1.0F,1.0F);
+                    return TypedActionResult.success(playerEntity.getStackInHand(hand));
+                }
             }
-        return TypedActionResult.fail(newArmor);
+        }
+        return TypedActionResult.pass(newArmor);
     }
 
 }
