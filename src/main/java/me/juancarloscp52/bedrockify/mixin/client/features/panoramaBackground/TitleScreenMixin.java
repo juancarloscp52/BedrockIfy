0076cfdc0cb677460497c49591e7f56d62b149c6
jc <juancarloscp52@gmail.com>
2021-06-08 20:54:40 +0200

    Fix getOverlay on production

--- src/main/java/me/juancarloscp52/bedrockify/mixin/client/features/panoramaBackground/TitleScreenMixin.java
+++ src/main/java/me/juancarloscp52/bedrockify/mixin/client/features/panoramaBackground/TitleScreenMixin.java
@@ -1,6 +1,9 @@
 package me.juancarloscp52.bedrockify.mixin.client.features.panoramaBackground;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.juancarloscp52.bedrockify.client.features.panoramaBackground.BedrockifyRotatingCubeMapRenderer;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.IceBlock;
 import net.minecraft.client.gui.RotatingCubeMapRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
@@ -11,10 +14,8 @@ import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 
 @Mixin(TitleScreen.class)
@@ -42,10 +43,18 @@ public abstract class TitleScreenMixin extends Screen {
         BedrockifyRotatingCubeMapRenderer.getInstance().render(MathHelper.clamp(f, 0.0F, 1.0F), true);
     }
 
-    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V", ordinal = 0))
-    private Identifier getOverlay(int i,Identifier id) {
-        BedrockifyRotatingCubeMapRenderer.getInstance().updateOverlayId(id);
-        return id;
+    @Group(name = "overlayID")
+    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V", ordinal = 0, remap = false),index = 1)
+    private Identifier getOverlayDev(Identifier identifier) {
+        BedrockifyRotatingCubeMapRenderer.getInstance().updateOverlayId(identifier);
+        return identifier;
+    }
+
+    @Group(name = "overlayID")
+    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/class_2960;)V", ordinal = 0,remap = false),index = 1)
+    private Identifier getOverlayProd(Identifier identifier) {
+        BedrockifyRotatingCubeMapRenderer.getInstance().updateOverlayId(identifier);
+        return identifier;
     }
 
 }
