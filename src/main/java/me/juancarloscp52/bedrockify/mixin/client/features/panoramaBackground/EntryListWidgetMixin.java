1065beb873d15e237aba076cdcbbab423cc7d959
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-12 03:13:20 +0200

    fixed Panorama ignore list

--- src/main/java/me/juancarloscp52/bedrockify/mixin/client/features/panoramaBackground/EntryListWidgetMixin.java
+++ src/main/java/me/juancarloscp52/bedrockify/mixin/client/features/panoramaBackground/EntryListWidgetMixin.java
@@ -17,28 +17,36 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntryListWidget.class)
 public abstract class EntryListWidgetMixin {
-    @Shadow @Final protected MinecraftClient client;
-    @Shadow protected int bottom;
-    @Shadow protected int top;
-    @Shadow protected abstract void renderBackground(MatrixStack matrices);
-    @Shadow public abstract void setRenderBackground(boolean renderBackground);
-    @Shadow public abstract void setRenderHorizontalShadows(boolean renderHorizontalShadows);
+    @Shadow
+    @Final
+    protected MinecraftClient client;
+    @Shadow
+    protected int bottom;
+    @Shadow
+    protected int top;
 
-    @Redirect(method = "render", at=@At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/EntryListWidget;renderBackground(Lnet/minecraft/client/util/math/MatrixStack;)V"))
-    private void renderPanorama(EntryListWidget entryListWidget, MatrixStack matrices){
-        if(!Bedrockify.getInstance().settings.isCubemapBackgroundEnabled() || shouldIgnoreScreen() /*|| this.client.currentScreen.getClass().getName().equals("net.minecraft.class_5522")*/) {
+    @Shadow
+    protected abstract void renderBackground(MatrixStack matrices);
+
+    @Shadow
+    public abstract void setRenderBackground(boolean renderBackground);
+
+    @Shadow
+    public abstract void setRenderHorizontalShadows(boolean renderHorizontalShadows);
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/EntryListWidget;renderBackground(Lnet/minecraft/client/util/math/MatrixStack;)V"))
+    private void renderPanorama(EntryListWidget entryListWidget, MatrixStack matrices) {
+        if (!Bedrockify.getInstance().settings.isCubemapBackgroundEnabled() || shouldIgnoreScreen() /*|| this.client.currentScreen.getClass().getName().equals("net.minecraft.class_5522")*/) {
             this.renderBackground(matrices);
             return;
         }
 
-        if (!(this.client.currentScreen instanceof PackScreen)) {
-            BedrockifyRotatingCubeMapRenderer.getInstance().render();
-            DrawableHelper.fill(matrices, 0, this.top, client.getWindow().getScaledWidth(), this.bottom, (100 << 24));
-        }
+        BedrockifyRotatingCubeMapRenderer.getInstance().render();
+        DrawableHelper.fill(matrices, 0, this.top, client.getWindow().getScaledWidth(), this.bottom, (100 << 24));
     }
 
-    @Inject(method = "<init>(Lnet/minecraft/client/MinecraftClient;IIIII)V", at=@At("RETURN"))
-    private void bedrockify$ctor(MinecraftClient client, int width, int height, int top, int bottom, int itemHeight, CallbackInfo ci){
+    @Inject(method = "<init>(Lnet/minecraft/client/MinecraftClient;IIIII)V", at = @At("RETURN"))
+    private void bedrockify$ctor(MinecraftClient client, int width, int height, int top, int bottom, int itemHeight, CallbackInfo ci) {
         // Prevent the screen background from drawing
         this.setRenderBackground(!Bedrockify.getInstance().settings.isCubemapBackgroundEnabled() || shouldIgnoreScreen());
         // Prevent top and bottom bars from drawing (Only on pack Screens)
