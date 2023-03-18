0c9c5101e038740e8f5d41c2f2b3470b2e19997f
jc <juancarloscp52@gmail.com>
2023-03-18 23:52:17 +0100

    Fix exp level color (fixes #238)

--- src/main/java/me/juancarloscp52/bedrockify/mixin/client/features/screenSafeArea/InGameHudMixin.java
+++ src/main/java/me/juancarloscp52/bedrockify/mixin/client/features/screenSafeArea/InGameHudMixin.java
@@ -8,7 +8,7 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -69,7 +69,7 @@ public abstract class InGameHudMixin extends DrawableHelper {
 
         if(color == 0)
             return 0;
-        return fontRenderer.drawWithShadow(matrices, text, x, y-screenBorder-3, MathHelper.packRgb(127, 252, 32) | (alpha << 24));
+        return fontRenderer.drawWithShadow(matrices, text, x, y-screenBorder-3, ColorHelper.Argb.getArgb(alpha,127, 252, 32));
     }
 
     /**
