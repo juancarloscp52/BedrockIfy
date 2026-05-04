7a8d409224c3cb160b954934ff8872de11073626
lonefelidae16 <kow161206@gmail.com>
2026-05-04 22:34:39 +0900

    Support custom item name
    in HeldItemTooltips functionality
    which has Container component

--- src/main/java/me/juancarloscp52/bedrockify/client/features/heldItemTooltips/tooltip/ContainerTooltip.java
+++ src/main/java/me/juancarloscp52/bedrockify/client/features/heldItemTooltips/tooltip/ContainerTooltip.java
@@ -1,23 +1,16 @@
 package me.juancarloscp52.bedrockify.client.features.heldItemTooltips.tooltip;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.PotionItem;
-import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.item.ItemStack;
 
 public class ContainerTooltip extends Tooltip {
     Component itemName;
 
     public ContainerTooltip(ItemStack itemStack){
         this.primaryValue = itemStack.getCount();
-
-        Item item = itemStack.getItem();
-        if (item instanceof PotionItem potionItem) {
-            this.itemName = potionItem.getName(itemStack);
-        } else {
-            this.itemName = Component.translatable(item.getDescriptionId());
-        }
+        this.itemName = itemStack.getOrDefault(DataComponents.CUSTOM_NAME, itemStack.getItemName());
     }
 
     @Override
